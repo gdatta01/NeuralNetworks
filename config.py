d2b3b9a1800131bdea8ff807458b4f52bee8b39e
Gaurav Datta <datta.gaurav@.gmail.com>
2021-08-08 13:22:04 -0700

    Move smoothing to dataset

--- config.py
+++ config.py
@@ -14,12 +14,12 @@ def datset_config():
     cfg.NAME = ""
     cfg.PATH = ""
     cfg.NORMALIZE = False
+    cfg.TARGET_SMOOTHING = 0.0
     return cfg
 
 
 def training_config():
     cfg = CfgNode()
-    cfg.TARGET_SMOOTHING = 0.0
     cfg.LOSS = ""
     cfg.EPOCHS = 0
     cfg.BATCH_SIZE = 0
